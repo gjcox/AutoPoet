6bec0007e770b29d7c5668d1c8fa408cc9b5860c
190021081 <gjc6@st-andrews.ac.uk>
2021-12-05 16:04:59 +0000

    SuperWord constructor and populator demos

--- src/testing/Demos.java
+++ src/testing/Demos.java
@@ -1,16 +1,17 @@
 package testing;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import apis.WordsAPI;
-import utils.JSONConstructors.Emphasis;
 import utils.Pair;
+import words.Emphasis;
 import words.IPAHandler;
-import words.Word;
+import words.SuperWord;
+import words.Syllable;
 
 public class Demos {
 
@@ -25,11 +26,11 @@ public class Demos {
         }
     }
 
-    private static void demoGetSyllables(String word, String ipa) {
-        Pair<JSONArray, JSONObject> pair = IPAHandler.getSyllables(ipa);
-        JSONArray syllables = pair.one();
-        JSONObject emphasis = pair.two();
-        System.out.println(word + ";" + ipa + ": ");
+    private static void demoGetSyllables(String SuperWord, String ipa) {
+        Pair<ArrayList<Syllable>, Emphasis> pair = IPAHandler.getSyllables(ipa);
+        ArrayList<Syllable> syllables = pair.one();
+        Emphasis emphasis = pair.two();
+        System.out.println(SuperWord + ";" + ipa + ": ");
         System.out.println(syllables.toString());
         System.out.println(emphasis.toString());
 
@@ -38,26 +39,34 @@ public class Demos {
     private static void demoRhymes() {
         List<String> words_1 = Arrays.asList("test", "test", "hideous", "attack", "whim", "painted", "painted",
                 "overstated", "stated", "fated", "zombie", "zombie");
-        List<String> ipa_words_1 = Arrays.asList("tɛst", "tɛst", "hɪdiəs", "ə'tæk", "wɪm", "'peɪntɪd", "'peɪntɪd",
-                "'oʊvɝr,steɪtɪd", "'steɪtɪd", "'feɪtɪd", "'zɑmbi", "'zɑmbi");
+        /*
+         * List<String> ipa_words_1 = Arrays.asList("tɛst", "tɛst", "hɪdiəs", "ə'tæk",
+         * "wɪm", "'peɪntɪd", "'peɪntɪd",
+         * "'oʊvɝr,steɪtɪd", "'steɪtɪd", "'feɪtɪd", "'zɑmbi", "'zɑmbi");
+         */
         List<String> words_2 = Arrays.asList("guest", "human", "insidious", "aback", "dim", "acquainted", "understated",
                 "understated", "understated", "understated", "abercrombie", "bee");
-        List<String> ipa_words_2 = Arrays.asList("ɡɛst", "'hjumən", "ɪn'sɪdiəs", "ə'bæk", "dɪm", "ə'kweɪntɪd",
-                "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd", "'æbər,krɑmbi", "bi");
+        /*
+         * List<String> ipa_words_2 = Arrays.asList("ɡɛst", "'hjumən", "ɪn'sɪdiəs",
+         * "ə'bæk", "dɪm", "ə'kweɪntɪd",
+         * "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd", "'ʌndɝr,steɪtɪd",
+         * "'æbər,krɑmbi", "bi");
+         */
 
         for (int i = 0; i < words_1.size(); i++) {
-            Word word_1 = new Word(words_1.get(i), ipa_words_1.get(i));
-            Word word_2 = new Word(words_2.get(i), ipa_words_2.get(i));
-            boolean rhymes = IPAHandler.checkRhyme(word_1, word_2);
-            System.out.println(String.format("\"%s\" rhymes with \"%s\": %b", words_1.get(i), words_2.get(i), rhymes));
+            SuperWord word_1 = SuperWord.getSuperWord(words_1.get(i));
+            SuperWord word_2 = SuperWord.getSuperWord(words_2.get(i));
+            // boolean rhymes = IPAHandler.checkRhyme(word_1, word_2);
+            // System.out.println(String.format("\"%s\" rhymes with \"%s\": %b",
+            // words_1.get(i), words_2.get(i), rhymes));
         }
     }
 
     public static void getIPA() {
         List<String> words = Arrays.asList("hideous", "monsters", "the", "wind", "winds");
-        for (String word : words) {
-            JSONObject ipa = WordsAPI.getIPA(word);
-            System.out.print(word + ": ");
+        for (String SuperWord : words) {
+            JSONObject ipa = WordsAPI.getIPA(SuperWord);
+            System.out.print(SuperWord + ": ");
             System.out.println(ipa.toString());
         }
     }
@@ -65,54 +74,41 @@ public class Demos {
     public static void wordConstructor() {
         List<String> words = Arrays.asList("hideous", "monsters", "the", "wind", "winds", "present", "understated");
         for (String word : words) {
-            Word word_object = new Word(word);
-            System.out.print(word + ": ");
-            System.out.println(word_object.toString());
+            SuperWord wordObject = SuperWord.getSuperWord(word);
+            System.out.println(wordObject.toString());
         }
     }
 
     public static void wordPopulator() {
-        List<String> words = Arrays.asList(/*"hideous", */"monsters"/*, "the", "wind", "winds", "present", "understated"*/);
+        List<String> words = Arrays.asList("hideous", "monsters", "the", "wind", "winds", "present",
+                "understated");
         for (String word : words) {
-            Word word_object = new Word(word);
-            word_object.populateSynonyms();
-            word_object.populateCommonTypes();
-            System.out.print(word + ": ");
-            System.out.println(word_object.toString());
+            SuperWord wordObject = SuperWord.getSuperWord(word);
+            wordObject.populate();
+            System.out.println(wordObject.toString());
         }
     }
 
     public static void presentPresent() {
-        Word present = new Word("present");
-        Word resent = new Word("resent");
-        boolean rhymes = IPAHandler.checkRhyme(present, resent);
-        System.out.println(
-                String.format("\"%s\" rhymes with \"%s\": %b", present.plaintext(), resent.plaintext(), rhymes));
+        SuperWord present = SuperWord.getSuperWord("present");
+        SuperWord resent = SuperWord.getSuperWord("resent");
+        // boolean rhymes = IPAHandler.checkRhyme(present, resent);
+        // System.out.println(String.format("\"%s\" rhymes with \"%s\": %b",
+        // present.getPlaintext(), resent.getPlaintext(), rhymes));
 
     }
 
     public static void rhymeLengths() {
-        Word understated = new Word("understated");
+        SuperWord understated = SuperWord.getSuperWord("understated");
         System.out.println(understated);
-        JSONObject rhyme_lengths = understated.rhymeLengths("all");
-        System.out.println(rhyme_lengths);
-    }
-
-    public static void newEmphasisObject() {
-        JSONObject object = Emphasis.newEmphasisObject();
-        System.out.println(object);
-        object.put(Emphasis.PRIMARY, 1);
-        System.out.println(object);
-        ((JSONArray) object.get(Emphasis.SECONDARY)).put(2);
-        System.out.println(object);
-        ((JSONArray) object.get(Emphasis.SECONDARY)).put(3);
-        System.out.println(object);
+        // JSONObject rhyme_lengths = understated.rhymeLengths("all");
+        // System.out.println(rhyme_lengths);
     }
 
     public static void abercrombieZombie() {
-        Word abercrombie = new Word("abercrombie");
-        Word zombie = new Word("zombie");
+        SuperWord abercrombie = SuperWord.getSuperWord("abercrombie");
         System.out.println(abercrombie);
+        SuperWord zombie = SuperWord.getSuperWord("zombie");
         System.out.println(zombie);
     }
 
@@ -122,14 +118,10 @@ public class Demos {
         // demoRhymes();
         // getIPA();
         // wordConstructor();
-        // wordPopulator();
+        wordPopulator();
         // presentPresent();
         // rhymeLengths();
         // newEmphasisObject();
         // abercrombieZombie();
-        
-        Word word_object = new Word("devil");
-        System.out.print("devil" + ": ");
-        System.out.println(word_object.toString());
     }
 }
