6e04c03550fc2c276933cf2805f0303c0c675b18
190021081 <gjc6@st-andrews.ac.uk>
2021-12-05 16:04:45 +0000

    Error corrections; Added toString

--- src/words/SuperWord.java
+++ src/words/SuperWord.java
@@ -1,14 +1,30 @@
 package words;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import apis.WordsAPI;
+
+import static utils.NullListOperations.addToNull;
+import static config.Configuration.LOG;
+
+/**
+ * TODO toString method
+ */
 public class SuperWord implements Comparable<SuperWord> {
 
+    private static HashMap<String, SuperWord> cachePopulated = new HashMap<>();
+    private static HashMap<String, SuperWord> cachePlaceholder = new HashMap<>();
+
     private String plaintext;
+    private boolean populated = false; // true iff built from a WordsAPI query
+    private Pronunciation pronunciation;
     private ArrayList<Word> nouns;
     private ArrayList<Word> pronouns;
     private ArrayList<Word> verbs;
@@ -16,69 +32,187 @@ public class SuperWord implements Comparable<SuperWord> {
     private ArrayList<Word> adverbs;
     private ArrayList<Word> prepositions;
     private ArrayList<Word> conjunctions;
-    private Pronunciation pronunciation = new Pronunciation();
+    private ArrayList<Word> definiteArticles;
+    private ArrayList<Word> unknowns;
 
     /**
+     * Attempts to get a cached word, before returning a new placeholder.
      * 
-     * @param word JSONObject from a word request. See WordsApiNotes.txt for
-     *             examples.
+     * @param plaintext
+     * @return
      */
-    public SuperWord(JSONObject word) {
-        if (word.has("word")) {
-            this.plaintext = word.getString("word");
+    public static SuperWord getSuperWord(String plaintext) {
+        if (cachePopulated.containsKey(plaintext)) {
+            LOG.writeTempLog(String.format("Retrieved populated superword for \"%s\" from cache.", plaintext));
+            return cachePopulated.get(plaintext);
         }
+        if (cachePlaceholder.containsKey(plaintext)) {
+            LOG.writeTempLog(String.format("Retrieved placeholder superword for \"%s\" from cache.", plaintext));
+            return cachePlaceholder.get(plaintext);
+        }
+        return new SuperWord(plaintext);
+    }
+
+    public void populate() {
+        if (populated) {
+            LOG.writeTempLog(String.format("Attempted to repopulate \"%s\": ", plaintext, this.toString()));
+            return;
+        }
+
+        JSONObject word = WordsAPI.getWord(plaintext);
+
+        if (word.has("word") && !word.getString("word").equals(plaintext)) {
+            LOG.writePersistentLog(String.format("WordsAPI responded with word \"%s\" when requesting \"%s\".",
+                    word.getString("word"), plaintext));
+        }
+
+        this.pronunciation = new Pronunciation();
 
         if (word.has("syllables")) {
             JSONObject syllablesObject = word.getJSONObject("syllables");
             this.pronunciation.setSyllables(syllablesObject);
+        } else {
+            LOG.writePersistentLog(String.format("Syllables of \"%s\" is missing", plaintext));
         }
 
         if (word.has("pronunciation")) {
-            JSONObject pronunciationObject = word.getJSONObject("pronunciation");
-            this.pronunciation.setIPA(pronunciationObject);
+            try {
+                JSONObject pronunciationObject = word.getJSONObject("pronunciation");
+                this.pronunciation.setIPA(pronunciationObject);
+            } catch (JSONException e) {
+                LOG.writePersistentLog(String.format("Pronunciation of \"%s\" is not a JSONObject: \"%s\"", plaintext,
+                        word.get("pronunciation").toString()));
+                this.pronunciation.setIPA(word.getString("pronunciation"));
+            }
         }
 
         if (word.has("results")) {
             JSONArray resultsArray = word.getJSONArray("results");
             this.setWords(resultsArray);
         }
+
+        populated = true;
+        cachePopulated.put(this.plaintext, this);
     }
 
-    private boolean addWord(List<Word> list, Word word) {
-        if (list == null) {
-            list = new ArrayList<>();
+    /**
+     * For creating placeholders
+     * 
+     * @param plaintext
+     */
+    private SuperWord(String plaintext) {
+        this.plaintext = plaintext;
+        cachePlaceholder.put(this.plaintext, this);
+    }
+
+    /**
+     * 
+     * @param plaintexts JSONArrays are not typed, but must be have String elements.
+     * @return
+     */
+    public static ArrayList<SuperWord> batchPlaceHolders(List<Object> plaintexts) {
+        ArrayList<SuperWord> list = new ArrayList<>();
+        for (Object plaintext : plaintexts) {
+            list.add(getSuperWord((String) plaintext));
         }
-        return list.add(word);
+        return list;
     }
 
+    @SuppressWarnings("unchecked")
     private void setWords(JSONArray resultsArray) {
         List<Object> results = resultsArray.toList();
         for (Object result : results) {
-            Word word = new Word((JSONObject) result);
+            Word word = new Word(plaintext, (Map<String, Object>) result);
             switch (word.partOfSpeech()) {
                 case NOUN:
-                    addWord(nouns, word);
+                    nouns = addToNull(nouns, word);
                     break;
                 case PRONOUN:
-                    addWord(pronouns, word);
+                    pronouns = addToNull(pronouns, word);
                     break;
                 case VERB:
-                    addWord(verbs, word);
+                    verbs = addToNull(verbs, word);
                     break;
                 case ADJECTIVE:
-                    addWord(adjectives, word);
+                    adjectives = addToNull(adjectives, word);
                     break;
                 case ADVERB:
-                    addWord(adverbs, word);
+                    adverbs = addToNull(adverbs, word);
                     break;
                 case PREPOSITION:
-                    addWord(prepositions, word);
+                    prepositions = addToNull(prepositions, word);
                     break;
                 case CONJUCTION:
-                    addWord(conjunctions, word);
+                    conjunctions = addToNull(conjunctions, word);
+                    break;
+                case DEFINITE_ARTICLE:
+                    definiteArticles = addToNull(definiteArticles, word);
+                    break;
+                case UNKNOWN:
+                    unknowns = addToNull(unknowns, word);
                     break;
             }
         }
+    }
+
+    public String getPlaintext() {
+        return plaintext;
+    }
+
+    public String toString() {
+        String divider = "\n\t";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(plaintext + ": {");
+        if (populated) {
+            stringBuilder.append(divider);
+        }
+        stringBuilder.append("populated: " + populated);
+        if (pronunciation != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("pronunciation: " + pronunciation.toString());
+        }
+        if (nouns != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("nouns: " + nouns.toString());
+        }
+        if (pronouns != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("pronouns: " + pronouns.toString());
+        }
+        if (verbs != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("verbs: " + verbs.toString());
+        }
+        if (adjectives != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("adjectives: " + adjectives.toString());
+        }
+        if (adverbs != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("adverbs: " + adverbs.toString());
+        }
+        if (prepositions != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("prepositions: " + prepositions.toString());
+        }
+        if (conjunctions != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("conjunction: " + conjunctions.toString());
+        }
+        if (definiteArticles != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("definiteArticles: " + definiteArticles.toString());
+        }
+        if (unknowns != null) {
+            stringBuilder.append(divider);
+            stringBuilder.append("unknowns: " + unknowns.toString());
+        }
+        if (populated) {
+            stringBuilder.append("\n");
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 
     /**
