da585c7fe189e10e4765cf24c4116421f1c7d92a
190021081 <gjc6@st-andrews.ac.uk>
2021-12-05 16:04:05 +0000

    Added toString; Ignore "all" if there are other pronunciations

--- src/words/Pronunciation.java
+++ src/words/Pronunciation.java
@@ -18,9 +18,21 @@ public class Pronunciation {
         String ipa;
         ArrayList<Syllable> syllables;
         Emphasis emphasis;
+
+        public String toString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("{");
+            stringBuilder.append("ipa: " + ipa);
+            stringBuilder.append(", ");
+            stringBuilder.append("syllables: " + syllables.toString());
+            stringBuilder.append(", ");
+            stringBuilder.append("emphasis: " + emphasis.toString());
+            stringBuilder.append("}");
+            return stringBuilder.toString();
+        }
     }
 
-    private ArrayList<String> syllables;
+    private ArrayList<String> syllables = new ArrayList<>();
 
     private SubPronunciation noun;
     private SubPronunciation pronoun;
@@ -60,8 +72,11 @@ public class Pronunciation {
      */
     public void setIPA(JSONObject pronunciationObject) {
         Pair<ArrayList<Syllable>, Emphasis> syllablesAndEmphasis;
+        boolean needsAll = true; // prevent adding and "all" category if there are other categories
 
         if (pronunciationObject.has("noun")) {
+            needsAll = false; 
+            this.noun = new SubPronunciation();
             this.noun.ipa = pronunciationObject.getString("noun");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.noun.ipa);
             this.noun.syllables = syllablesAndEmphasis.one();
@@ -69,6 +84,8 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("pronoun")) {
+            needsAll = false; 
+            this.pronoun = new SubPronunciation();
             this.pronoun.ipa = pronunciationObject.getString("pronoun");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.pronoun.ipa);
             this.pronoun.syllables = syllablesAndEmphasis.one();
@@ -76,6 +93,8 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("verb")) {
+            needsAll = false; 
+            this.verb = new SubPronunciation();
             this.verb.ipa = pronunciationObject.getString("verb");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.verb.ipa);
             this.verb.syllables = syllablesAndEmphasis.one();
@@ -83,6 +102,8 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("adjective")) {
+            needsAll = false; 
+            this.adjective = new SubPronunciation();
             this.adjective.ipa = pronunciationObject.getString("adjective");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.adjective.ipa);
             this.adjective.syllables = syllablesAndEmphasis.one();
@@ -90,6 +111,8 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("adverb")) {
+            needsAll = false; 
+            this.adverb = new SubPronunciation();
             this.adverb.ipa = pronunciationObject.getString("adverb");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.adverb.ipa);
             this.adverb.syllables = syllablesAndEmphasis.one();
@@ -97,6 +120,8 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("preposition")) {
+            needsAll = false; 
+            this.preposition = new SubPronunciation();
             this.preposition.ipa = pronunciationObject.getString("preposition");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.preposition.ipa);
             this.preposition.syllables = syllablesAndEmphasis.one();
@@ -104,19 +129,30 @@ public class Pronunciation {
         }
 
         if (pronunciationObject.has("conjunction")) {
+            needsAll = false; 
+            this.conjunction = new SubPronunciation();
             this.conjunction.ipa = pronunciationObject.getString("conjunction");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.conjunction.ipa);
             this.conjunction.syllables = syllablesAndEmphasis.one();
             this.conjunction.emphasis = syllablesAndEmphasis.two();
         }
 
-        if (pronunciationObject.has("all")) {
+        if (needsAll && pronunciationObject.has("all")) {
+            this.all = new SubPronunciation();
             this.all.ipa = pronunciationObject.getString("all");
             syllablesAndEmphasis = IPAHandler.getSyllables(this.all.ipa);
             this.all.syllables = syllablesAndEmphasis.one();
             this.all.emphasis = syllablesAndEmphasis.two();
         }
 
+    }
+
+    public void setIPA(String all) {
+        this.all = new SubPronunciation();
+        this.all.ipa = all;
+        Pair<ArrayList<Syllable>, Emphasis> syllablesAndEmphasis = IPAHandler.getSyllables(this.all.ipa);
+        this.all.syllables = syllablesAndEmphasis.one();
+        this.all.emphasis = syllablesAndEmphasis.two();
     }
 
     public SubPronunciation getNoun() {
@@ -163,6 +199,46 @@ public class Pronunciation {
 
     public SubPronunciation getAll() {
         return this.all;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+        stringBuilder.append("plaintext-syllables: " + syllables.toString());
+        if (noun != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("noun: " + noun.toString());
+        }
+        if (pronoun != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("pronoun: " + pronoun.toString());
+        }
+        if (verb != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("verb: " + verb.toString());
+        }
+        if (adjective != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("adjective: " + adjective.toString());
+        }
+        if (adverb != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("adverb: " + adverb.toString());
+        }
+        if (preposition != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("preposition: " + preposition.toString());
+        }
+        if (conjunction != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("conjunction: " + conjunction.toString());
+        }
+        if (all != null) {
+            stringBuilder.append(", ");
+            stringBuilder.append("all: " + all.toString());
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 
 }
