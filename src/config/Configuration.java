a987606421f433dd3d6424af88383a22e382ee87
190021081 <gjc6@st-andrews.ac.uk>
2022-03-27 15:58:22 +0100

    Added JavaDoc comments

--- src/config/Configuration.java
+++ src/config/Configuration.java
@@ -2,6 +2,11 @@ package config;
 
 import utils.LogWriter;
 
+/**
+ * This class is simply to have a centrally defined log writer.
+ * 
+ * @author 190021081
+ */
 public abstract class Configuration {
 
     public static final LogWriter LOG = new LogWriter(true);
--- src/exceptions/RhymingSchemeSizeException.java
+++ src/exceptions/RhymingSchemeSizeException.java
@@ -1,9 +1,15 @@
 package exceptions;
 
+/**
+ * An extension of the Exception class, for catching when a stanza is assigned a
+ * rhyme scheme of incorrect length.
+ * 
+ * @author 190021081
+ */
 public class RhymingSchemeSizeException extends Exception {
-    
+
     public RhymingSchemeSizeException(String message) {
-        super(message); 
+        super(message);
     }
 
 }
